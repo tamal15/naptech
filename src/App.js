62c198cf52a9d045b47d0585632d0b772366898f
tamal15 <tamal15-13243@diu.edu.bd>
2022-06-13 13:10:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Router>
       {/* <Header /> */}
       <Routes>
-        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Home />} />
       
        
       
